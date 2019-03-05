f0d530b1446e0e9029478dd8c9a752f35975c8c3
Jeff Day <jeffrey.day00@gmail.com>
2019-03-05 15:40:41 -0800

    completed Underbar 'extend'

--- src/underbar.js
+++ src/underbar.js
@@ -256,11 +256,18 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+     _.each(arguments, function(x){
+         for (var key in x){
+             obj[key] = x[key];
+         }
+     });
+      return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+      
   };
 
 
